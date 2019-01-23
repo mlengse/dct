85f58a55b8ede1902129514301eb491e8cf248c7
mlengse <anjangkn@gmail.com>
2019-01-23 12:35:57 +0700

    update mutu

--- pages/indikator-mutu/index.js
+++ pages/indikator-mutu/index.js
@@ -5,18 +5,35 @@ const apiUrl = process.env.apiUrl || 'http://localhost:1337'
 const strapi = new Strapi(apiUrl)
 
 export default {
+    data: () => ({
+        sortBy: 'bagian',
+        sortDesc: false,
+        query: ""
+    }),
+    methods: {
+        rowHover: (item, index, event) => console.log(JSON.stringify(item))
+    },
     computed: {
+        filteredMutu() {
+            return this.mutus.filter(mutu => JSON.stringify(mutu).toLowerCase().includes(this.query.toLowerCase()))
+        },
+        fields() {
+            return Object.keys(this.mutus[0]).map(e => ({ 
+                key: e,
+                sortable: true
+            }))
+        },
         mutus() {
-            return this.$store.getters['mutus/list']
+            return this.$store.getters["mutus/list"];
         }
     },
     fetch: async ({ store }) => {
-        const { data: { states } } = await strapi.request(
-          "post",
-          "/graphql",
-          {
+        store.commit("mutus/emptyList");
+        const {
+            data: { states }
+        } = await strapi.request("post", "/graphql", {
             data: {
-              query: `query {
+                query: `query {
                         states ( where: {
                         name: "mutu"
                     }) {
@@ -26,7 +43,7 @@ export default {
                         counternames {
                             name
                             countertype {
-                            name
+                                name
                             }
                         }
                         operator {
@@ -43,20 +60,26 @@ export default {
                     }
                 }`
             }
-          }
-        );
+            });
 
-        const {indicators} = states[0]
-
-        indicators.forEach(indicator => {
-            store.commit('mutus/add', {
+        const { indicators } = states[0];
+        for(let i=0; i< indicators.length; i++) {
+            const indicator = indicators[i]
+            let push = {}
+            if (indicator.counternames.length) {
+                for (let counter of indicator.counternames) {
+                    push[counter.countertype.name] = counter.name
+                }
+            }
+            store.commit("mutus/add", Object.assign({
                 bagian: indicator.bagian.nama,
                 nama: indicator.name,
+            }, push, {
                 operator: indicator.operator.name,
-                target: indicator.target,
-                satuan: indicator.satuan.name,
-
-            })
-        })
+                target: `${indicator.operator.name === '<=' ? `<= ` : ``}${indicator.target} ${indicator.satuan.name === 'persen' ? '%' : indicator.satuan.name}`,
+                satuan: indicator.satuan.name
+            }));
+        }
     }
-}
+
+};
--- pages/indikator-mutu/index.vue
+++ pages/indikator-mutu/index.vue
@@ -2,10 +2,16 @@
   section.container 
     .row
       .col-md-12
-        h1 Indikator Mutu
+        h3 Indikator Mutu
+
     .row
       .col-md-12
-        b-table(striped hover :items="mutus")
+        .form-group.mt-3
+          input.form-control(v-model='query' type='text' placeholder='Search...')
+
+    .row
+      .col-md-12
+        b-table(striped hover :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" :fields="fields" :items="filteredMutu" @row-hovered="rowHover")
 </template>
 
 <script src='./index.js'>
