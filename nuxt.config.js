6c109bd242cce9c4086a952c06627f82f913fcb7
mlengse <anjangkn@gmail.com>
2019-01-26 10:53:02 +0700

    undo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,60 +1,54 @@
 module.exports = {
-  env: {
-    apiUrl: process.env.apiUrl
-  },
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'Puskesmas Sibela',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'aplikasi bantu Puskesmas Sibela' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
-    //analyze: true,
-    /*
-    ** Run ESLint on save
-    */
-    babel: {
-      plugins: ['babel-plugin-inline-import']
-    },
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  },
-  plugins:[
-    {
-      src: '~/plugins/vue-bootstrap',
-      ssr: false
-
-    }
-  ],
-  modules: [
-    '@nuxtjs/dotenv',
-    ['@nuxtjs/moment', {
-      locales: ['id'],
-      defaultLocale: 'id'
-    }]
-  ]
+	env: {
+		apiUrl: process.env.apiUrl
+	},
+	/*
+	** Headers of the page
+	*/
+	head: {
+		title: 'Puskesmas Sibela',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: 'aplikasi bantu Puskesmas Sibela' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		]
+	},
+	/*
+	** Customize the progress bar color
+	*/
+	loading: { color: '#3B8070' },
+	/*
+	** Build configuration
+	*/
+	build: {
+		//analyze: true,
+		/*
+		** Run ESLint on save
+		*/
+		babel: {
+			plugins: ['babel-plugin-inline-import']
+		},
+		extend (config, { isDev, isClient }) {
+			if (isDev && isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+		}
+	},
+	modules: [
+		'bootstrap-vue/nuxt',
+		'@nuxtjs/dotenv',
+		['@nuxtjs/moment', {
+			locales: ['id'],
+			defaultLocale: 'id'
+		}]
+	]
 }
 
--- plugins/vue-bootstrap.js
+++ plugins/vue-bootstrap.js
@@ -12,9 +12,6 @@ import {
   Table
 } from 'bootstrap-vue/es/components';
 
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-
 [
   Alert,
   Layout,
