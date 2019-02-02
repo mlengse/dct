7285e8e3c58b4cc5b25078723059200dc4f2591e
mlengse <anjangkn@gmail.com>
2019-02-02 11:07:24 +0700

    temporary

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,9 @@
 export default {
-  apiKey: "AIzaSyBM6yjCp1G78XGm7vYgcmaq31troIO80Is",
-  authDomain: "sibela-3b205.firebaseapp.com",
-  databaseURL: "https://sibela-3b205.firebaseio.com",
-  projectId: "sibela-3b205",
-  storageBucket: "sibela-3b205.appspot.com",
-  messagingSenderId: "1086782614670"
+  apiUrl: process.env.apiUrl,
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId
 };
--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,13 @@ module.exports = {
 	],
 
 	env: {
-		apiUrl: process.env.apiUrl
+		apiUrl: process.env.apiUrl,
+		apiKey: process.env.apiKey,
+		authDomain: process.env.authDomain,
+		databaseURL: process.env.databaseURL,
+		projectId: process.env.projectId,
+		storageBucket: process.env.storageBucket,
+		messagingSenderId: process.env.messagingSenderId
 	},
 
 	/*
