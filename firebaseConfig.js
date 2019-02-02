a2a09594ab054872f8d409230abdd57d8d61130c
mlengse <anjangkn@gmail.com>
2019-02-02 11:10:00 +0700

    temp

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiUrl: process.env.apiUrl,
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId
+};
