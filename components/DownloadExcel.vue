559eb82beaf6232889e466876470cde315a783ec
mlengse <anjangkn@gmail.com>
2019-02-23 02:15:45 +0700

    lazy load xlsx

--- components/DownloadExcel.vue
+++ components/DownloadExcel.vue
@@ -3,13 +3,18 @@ button.btn.btn-primary.btn-sm(type='button' @click='exportXLSX') {{label}}
 </template>
 
 <script>
-import { utils, writeFile } from 'xlsx'
+
 
 export default {
   props: ['data', 'fields', 'name', 'label'],
   methods:{
     exportXLSX(){
       var data = []
+      const { json_to_sheet, book_new, book_append_sheet, writeFile }  = () => import('xlsx').then(({ utils: {
+        json_to_sheet,
+        book_new,
+        book_append_sheet
+      }, writeFile }) => ({ json_to_sheet, book_new, book_append_sheet, writeFile }))
       this.data.map( e => {
         let obj = {}
         Object.keys(this.fields).map( a =>{
@@ -18,9 +23,9 @@ export default {
         data.push(obj)
       })
      // console.log(JSON.stringify(data, null, 2))
-      var WS = utils.json_to_sheet(data);
-      var wb = utils.book_new()
-      utils.book_append_sheet(wb, WS, this.name.split('.')[0])
+      var WS = json_to_sheet(data);
+      var wb = book_new()
+      book_append_sheet(wb, WS, this.name.split('.')[0])
       writeFile(wb, this.name)
     }
   }
