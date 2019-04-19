b7bf22028d7327cc0fd566b177f40cfa4ef6475d
sibela <uptpkmsibela@gmail.com>
2019-04-20 00:22:32 +0700

    change posyandu from table to list group

--- pages/posyandu-balita/index.vue
+++ pages/posyandu-balita/index.vue
@@ -7,55 +7,30 @@ section.container
 			.form-group.mt-3
 				input.form-control(v-model='query' type='text' placeholder='Search...')
 	no-ssr
-		.row.mt-2.fluid
-			b-table(
-				small
-				responsive 
-				hover 
-				selectable
-				select-mode='single'
-				stacked='sm' 
-				:busy.sync='loaded' 
-				:items="items"
-				:fields='fields' 
-				:filter='query'
-				:current-page="currentPage" 
-				:per-page="perPage" 
-				:sort-by.sync="sortBy" 
-				:sort-desc.sync="sortDesc" 
-				@filtered='onFiltered'
-				@row-selected='rowSelected'
-			)
 		.row.mt-2
-		.col-md-12
-			b-pagination(:total-rows="totalRows" :per-page="perPage" v-model="currentPage")
+			.col
+				b-list-group
+					b-list-group-item(v-for='menu in odd' :key='menu._key' button @click='click(menu)') {{ menu.name }} RW {{ menu.rw }}
+			.col
+				b-list-group
+					b-list-group-item(v-for='menu in even' :key='menu._key' button @click='click(menu)') {{ menu.name }} RW {{ menu.rw }}
 </template>
 
 <script>
 import getPosyanduGql from '~/apollo/queries/getPosyandu.gql'
 export default {
 	components: {
-		BTable: () => import('~/node_modules/bootstrap-vue/es/components/table/table'),
-		BPagination: () => import('~/node_modules/bootstrap-vue/es/components/pagination/pagination')
+		BListGroup: () => import('~/node_modules/bootstrap-vue/es/components/list-group/list-group'),
+		BListGroupItem: () => import('~/node_modules/bootstrap-vue/es/components/list-group/list-group-item')
 	},
+	
 	data: () => ({
 		loaded: false,
-		query: null,
-		sortBy: null,
-		sortDesc: false,
-		perPage: 10,
-		currentPage: 1,
-		totalRows: 0,
- 		fields:{
-			name: {
-				label: 'Nama',
-			},
-			rw: {
-				label: 'RW',
-			},
-		},
+		query: '',
 		items: []
+
 	}),
+
 	mounted() {
 		this.$nextTick(()=>{
 			this.$nuxt.$loading.start()
@@ -70,25 +45,28 @@ export default {
 			})
 		})
 	},
-	watch: {
-		items(val) {
-			this.totalRows = val.length
-		},
-	},
+
 	methods: {
-		lowerCase(string) {
-			return string.split(' ').map(e=> {
-				return e.charAt(0) + e.slice(1).toLowerCase();
-			}).join(' ')
-		},
-		onFiltered(filteredItems) {
-			this.totalRows = filteredItems.length
-			this.currentPage = 1
-		},
-		rowSelected(items){
-			this.$router.push(`/posyandu-balita/detail?id=${items[0].rw.toLowerCase().split(' ').join('-')}`)
+		click(menu) {
+			this.$router.push(`/posyandu-balita/detail?id=${menu.rw.toLowerCase().split(' ').join('-')}`)
 		}
 	},
+	
+	computed: {
+		odd(){
+			return this.filteredList.filter( (e, i) => (i+2)%2 == 0)
+		},
+		even(){
+			return this.filteredList.filter( (e, i) => (i+2)%2 !== 0)
+		},
+		filteredList() {
+			if(this.items.length){
+				return this.items.filter( item => JSON.stringify(item).toLowerCase().includes(this.query.toLowerCase()))
+			}
+			return []
+		},
+	},
+
 }
 </script>
 
